f847bce41f4c621915f9a272a57d9360380f4ae9
JingHuiTong <jhtong33@gmail.com>
2022-06-24 18:13:25 +0800

    6/24

--- TomoD_PiercePoint/Plot.py
+++ TomoD_PiercePoint/Plot.py
@@ -7,15 +7,13 @@ df = pd.read_csv(stafile)
 # print(df)
 for i in range(len(df['network'])):
     net = df['network'].values[i]
-    if net=='AB':
-        sta = df['station'].values[i]
-        stlat = df['lat'].values[i]
-        stlon = df['lon'].values[i]
-        print(sta)  
-    
-        stlat_text = stlat-1.2
-        cmd = '''
-        csh TomoD_PiercePoint.gmt %(sta)s %(stlat)f %(stlon)f %(stlat_text)f
-        '''%locals()
-        os.system(cmd)   
-    
+    sta = df['station'].values[i]
+    stlat = df['lat'].values[i]
+    stlon = df['lon'].values[i]
+    print(sta)  
+
+    stlat_text = stlat-1.2
+    cmd = '''
+    csh TomoD_PiercePoint.gmt %(sta)s %(stlat)f %(stlon)f %(stlat_text)f
+    '''%locals()
+    os.system(cmd)   
